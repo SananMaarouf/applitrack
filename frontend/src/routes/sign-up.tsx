bd766a2f5c3a504f3846aa8e7db6d2e08c69415d
Sanan Maarouf <sanan.adnan97@gmail.com>
2026-02-04 10:48:47 +0100

    refactor: simplify SignUp component by removing unnecessary props

--- frontend/src/routes/sign-up.tsx
+++ frontend/src/routes/sign-up.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute('/sign-up')({
 function SignUpPage() {
   return (
     <div className="flex justify-center">
-      <SignUp routing="path" path="/sign-up" />
+      <SignUp />
     </div>
   )
 }
