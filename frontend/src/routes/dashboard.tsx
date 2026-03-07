a230799a5e6c5242abf346b479beeafc625357ee
Sanan Maarouf <sanan.adnan97@gmail.com>
2026-03-08 00:55:54 +0100

    changed job application form container width. trimmed code

--- frontend/src/routes/dashboard.tsx
+++ frontend/src/routes/dashboard.tsx
@@ -59,12 +59,8 @@ function Dashboard() {
   return (
     <div className="space-y-6 max-w-6xl mx-auto my-8">
       <section className="w-full grid grid-cols-1 md:grid-cols-2 gap-6 mx-auto">
-        <section className="w-full">
           <Chart />
-        </section>
-        <section className="w-full">
           <JobApplicationForm user_id={userId} />
-        </section>
       </section>
 
       <section className='w-full mx-auto'>
